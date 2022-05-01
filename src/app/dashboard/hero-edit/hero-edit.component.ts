b8dcc38fce7abdf21b900d828cd571f1ac821950
Hector Mosquera <hectormosq@gmail.com>
2022-05-01 13:29:51 +0200

    removing unnecessary destroy

--- src/app/dashboard/hero-edit/hero-edit.component.ts
+++ src/app/dashboard/hero-edit/hero-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { BaseComponent } from '@app/core/components/abstract/base/base.component';
 import { BackendApiService } from '@app/core/services/backend-api.service';
@@ -9,12 +15,14 @@ import { HeroEditService } from './hero-edit.service';
   templateUrl: './hero-edit.component.html',
   styleUrls: ['./hero-edit.component.scss'],
 })
-export class HeroEditComponent extends BaseComponent implements OnInit, OnDestroy {
+export class HeroEditComponent
+  extends BaseComponent
+  implements OnInit, OnDestroy
+{
   @Output() saved = new EventEmitter();
 
   editing = false;
   heroForm: FormGroup;
-  subscrption: any;
 
   constructor(
     private _heroEditService: HeroEditService,
@@ -33,22 +41,18 @@ export class HeroEditComponent extends BaseComponent implements OnInit, OnDestro
 
   save() {
     const formValues = this.heroForm.getRawValue();
-    this.subscrption = this._backApi
+    const saveSubscription = this._backApi
       .saveHero(formValues)
       .subscribe((response) => {
-        this.heroForm.reset()
+        this.heroForm.reset();
         this.editing = false;
-        this.saved.emit()
+        this.saved.emit();
       });
+
+    this._subscriptions.push(saveSubscription);
   }
 
   private _buildForm() {
     this.heroForm = this._heroEditService.buildFormGroup();
   }
-
-  override ngOnDestroy() {
-    console.log(this.subscrption);
-  }
-
-
 }
